a9a7bc6951bb1274b5741ca6a1bef89925c9069e
Jan Mussler <jan.mussler@zalando.de>
2015-09-15 16:34:40 +0200

    Point to right registry

---
+++ create_squashed.sh
@@ -0,0 +1,5 @@
+export IMAGE=zmon-eventlog-service
+
+docker build -t registry.opensource.zalan.do/stups/$IMAGE:$1-unsquashed .
+docker save registry.opensource.zalan.do/stups/$IMAGE:$1-unsquashed | docker-squash -verbose -t registry.opensource.zalan.do/stups/$IMAGE:$1 | docker load
+docker push registry.opensource.zalan.do/stups/$IMAGE:$1
